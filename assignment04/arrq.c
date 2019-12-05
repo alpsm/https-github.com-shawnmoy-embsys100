f435953ba8468d5ba7587a956d06837eca615a45
Shawn Moy <shawnres@comcast.net>
2019-12-05 09:41:36 -0800

    fifo postal

---
+++ assignment04/arrq.c
@@ -0,0 +1,34 @@
+/*Fill-In Fill-Out,
+Condensed,
+12-05-19, smoy*/
+
+#include <stdio.h>
+
+int main(){
+
+int array[3]={5,2,9};                //Declare and initialize array.
+
+int backIndicate;                    //Declare variable. 
+int *frontIndicate;                  //Declare pointer.
+frontIndicate = &array[0];           //Initialize element.
+
+for(int i=0; i<3; i++){              //Initialize elements.
+	backIndicate=array[i];	
+        printf("%d ", backIndicate);
+}	
+printf("\n");
+
+while(*frontIndicate){               //Trace elements.
+	printf("%d ", *frontIndicate);
+	frontIndicate++;
+	backIndicate=0;
+	}
+printf("\n");
+
+for(int i=0; i<3; i++){              //Retrace elements.
+	printf("%d ", *frontIndicate);
+	}
+printf("\n");
+return 0;
+}
+	
