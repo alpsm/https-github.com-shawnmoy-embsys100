7c5a37f99f9083fe1ec7bab8d74b501cd9bb909a
Shawn Moy <shawnres@comcast.net>
2019-12-05 16:51:55 -0800

    fifo postal

---
+++ assignment04/dexq.c
@@ -0,0 +1,37 @@
+/*Fill-In Fill-Out,
+Condensed,
+12-05-19, smoy*/
+
+#include <stdio.h>
+
+int main(){
+
+int array[3]={5,2,14};                               //Declare and initialize array.
+int i=0;                                             //Declare variable.
+int backIndicate;                                    //Declare variable. 
+int *frontIndicate;                                  //Declare pointer.
+frontIndicate = &array[0];                           //Declare and initialize element.
+
+for(i=0; i<3; i++){                                  //Initialize elements.
+	backIndicate=array[i];	
+        printf("%d ", backIndicate);
+}	
+printf("\n");
+
+for(i=0, backIndicate=0;  i<3; i++, backIndicate++){ //Trace elements.
+	printf("%d ", *frontIndicate);
+	++frontIndicate;
+	array[backIndicate]=0;	
+	printf("%d ", array[backIndicate]);
+	}
+printf("\n");
+
+for(i=0; i<3; i++){                                  //Retrace elements.
+	printf("%d ", *frontIndicate);
+	}
+printf("\n");
+printf("%d %d %d", array[0], array[1], array[3]);    //Zero indices.
+printf("\n");
+return 0;
+}
+	
